ada00b64b2eaa05959cf48117fe0dca408dcbae2
Ari <Unknown User@ARI>
2011-07-09 12:37:37 -0600

    Worked on shapetool

--- SharePaint/src/sharePaint/models/ShapeTool.java
+++ SharePaint/src/sharePaint/models/ShapeTool.java
@@ -1,17 +1,23 @@
 package sharePaint.models;
 
+import java.awt.Color;
+
+import sharePaint.actions.RectangleAction;
+
 public class ShapeTool
 {
 	private Canvas canvas;
-
-	public ShapeTool(Canvas canvas)
+	private Color color;
+	
+	public ShapeTool(Canvas canvas, Color color)
 	{
 		this.canvas = canvas;
+		this.color = color;
 	}
-	
+
 	public void drawRect(int x, int y, int width, int height)
 	{
-		
+		canvas.draw(new RectangleAction(color, x, y, width, height));
 	}
 	
 	public void drawOval(int x, int y, int width, int height)
