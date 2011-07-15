42c11edecc5e0262aff585d1fdde0cb6212ee677
JamieWohletz <reflectiveresistance@gmail.com>
2011-07-14 19:07:22 -0600

    Added Rectangle Draw Capacity. Works in all directions. Duplicate rectangles still present.

--- SharePaint/src/sharePaint/models/ShapeTool.java
+++ SharePaint/src/sharePaint/models/ShapeTool.java
@@ -8,21 +8,48 @@ public class ShapeTool
 {
 	private Canvas canvas;
 	private Color color;
+	private int brushSize = 10;
+	private int formerX, formerY, formerWidth, formerHeight;
 	
-	public ShapeTool(Canvas canvas, Color color)
+	public ShapeTool(Canvas canvas, Color color, int brushSize)
 	{
 		this.canvas = canvas;
 		this.color = color;
+		this.brushSize = brushSize;
 	}
-
+	
+	public void setPenSize(int size)
+	{
+		brushSize = size;
+	}
+	
 	public void drawRect(int x, int y, int width, int height)
 	{
-		canvas.draw(new RectangleAction(color, x, y, width, height));
+		
+		canvas.draw(new RectangleAction(color, brushSize, x, y, width, height));
+	}
+	
+	public void eraseFormerRectangle()
+	{
+		canvas.draw(new RectangleAction(Color.WHITE, brushSize, formerX, formerY, formerWidth, formerHeight));
+	}
+	
+	public void setFormerRectCharacteristics(int formerX, int formerY, int formerWidth, int formerHeight)
+	{
+		this.formerX = formerX; 
+		this.formerY = formerY;
+		this.formerWidth = formerWidth;
+		this.formerWidth = formerHeight;
 	}
 	
 	public void drawOval(int x, int y, int width, int height)
 	{
 		
+	}
+	
+	public void setColor(Color c)
+	{
+		color = c;
 	}
 	
 }
